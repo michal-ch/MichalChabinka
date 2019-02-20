188917fbfec5ffacc9facbf45e643a39a2419abf
michal-ch <47789546+michal-ch@users.noreply.github.com>
2019-02-20 11:48:45 +0100

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,9 +0,0 @@
-dic={"name":"Michal","ageM":"27","country":"Wieden","girl":"Karina","ageK":"26"}
-print(list(dic.keys()))
-print("\n","**********************************************************","\n")
-m=input("Powyzej wyswietlona zostala biblioteka. Jesli chcesz przejsc dalej nacisnij: Enter")
-print(dic['name'],"and",dic['girl'],"live in:",dic['country'])
-print(dic['name'],"is",dic['ageM'],"and",dic['girl'],"is",dic['ageK'])
-print("\n","**********************************************************","\n")
-e=input("Jesli chcesz wyjsc, wcisnij: 2x Enter")
-print(exit())
